15f09f1e1227149e77af594cf2411639d7bba44b
Ataullah Arabzai <ataullahforpc@gmail.com>
2026-07-18 12:15:26 +0430

    scrolling issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { Outlet } from 'react-router-dom'
-import './App.css'
-import Navebar from './Header/Navebar'
-import Footer from './Footer/Footer'
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Navebar from "./Header/Navebar";
+import Footer from "./Footer/Footer";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
-
   return (
     <>
+      <ScrollToTop />
       <Navebar />
       <Outlet />
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
