0dd5d5374f47255dddba3ac6dc299d192f861bd7
Ataullah Arabzai <ataullahforpc@gmail.com>
2026-07-21 16:37:43 +0430

    dark mode added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -18,23 +18,30 @@ function Projects() {
   return (
     <main className="p-5 md:p-15 mt-5">
       <div>
-        <p data-aos='fade-right' className="text-3xl md:text-7xl font-bold">
-          My <span className="text-[#6e06f2] dark:text-orange-600">Best</span> Creations
+        <p data-aos="fade-right" className="text-3xl md:text-7xl font-bold">
+          My <span className="text-[#6e06f2] dark:text-orange-600">Best</span>{" "}
+          Creations
           <span className="font-bold text-3xl md:text-8xl text-[#6e06f2] dark:text-orange-600">
             .
           </span>
         </p>
-        <p data-aos='fade-left' className="text-[#4e525a] dark:text-gray-300 text-[16px] md:text-[24px] mt-5">
+        <p
+          data-aos="fade-left"
+          className="text-[#4e525a] dark:text-gray-300 text-[16px] md:text-[24px] mt-5"
+        >
           Designing and developing modern web applications that combine
           creativity,
         </p>
-        <p data-aos='fade-left' className="text-[#4e525a] dark:text-gray-300 text-[16px] md:text-[24px]">
+        <p
+          data-aos="fade-left"
+          className="text-[#4e525a] dark:text-gray-300 text-[16px] md:text-[24px]"
+        >
           performance, and exceptional user experiences.
         </p>
       </div>
 
       <section className="projects p-5">
-        <div data-aos='fade-up' className="project1 mt-10 bg-white rounded-2xl">
+        <div data-aos="fade-up" className="project1 mt-10 bg-white rounded-2xl dark:bg-gray-800 p-3">
           <div className="w-full shadow shadow-gray-400 p-5 flex justify-center items-center rounded-2xl">
             <a
               href="https://ataullaharabzai.github.io/Dental-Clinic/"
@@ -52,16 +59,16 @@ function Projects() {
           <p className="text-2xl md:text-5xl mt-3 font-semibold">
             Dental Clinic
           </p>
-          <p className="md:text-[22px] text-[#4e525a]">
+          <p className="md:text-[22px] dark:text-gray-300 text-[#4e525a]">
             Professional dental clinic web application with user Auth and
             responsive layout.
           </p>
-          <p className="md:text-[22px] text-[#4e525a]">
+          <p className="md:text-[22px] dark:text-gray-300 text-[#4e525a]">
             Build with HTML5, CSS3 and JavaScript.
           </p>
         </div>
 
-        <div data-aos='fade-up' className="project2 mt-10 bg-white rounded-2xl">
+        <div data-aos="fade-up" className="project2 mt-10 dark:bg-gray-800 p-3 bg-white rounded-2xl">
           <div className=" w-full shadow shadow-gray-400 p-5 flex justify-center items-center rounded-2xl">
             <a href="https://dev-hire-jobs.vercel.app" className="">
               <div className="shadow shadow-gray-400 rounded-2xl">
@@ -74,16 +81,16 @@ function Projects() {
             </a>
           </div>
           <p className="text-2xl md:text-5xl mt-3 font-semibold">Jobs Board</p>
-          <p className="md:text-[22px] text-[#4e525a]">
+          <p className="md:text-[22px] text-[#4e525a] dark:text-gray-300">
             A job board web application for developers, designers and other tech
             users.
           </p>
-          <p className="md:text-[22px] text-[#4e525a]">
+          <p className="md:text-[22px] text-[#4e525a] dark:text-gray-300">
             Build with React, TypeScript and TailwindCSS.
           </p>
         </div>
 
-        <div data-aos='fade-up' className="project3 mt-20 bg-white rounded-2xl">
+        <div data-aos="fade-up" className="project3 mt-20 dark:bg-gray-800 p-3 bg-white rounded-2xl">
           <div className="w-full shadow shadow-gray-400 p-5 flex justify-center items-center rounded-2xl">
             <a href="https://github.com/ataullaharabzai" className="">
               <div>
@@ -98,15 +105,15 @@ function Projects() {
           <p className="text-2xl md:text-5xl mt-3 font-semibold">
             Shopping Store
           </p>
-          <p className="md:text-[22px] text-[#4e525a]">
+          <p className="md:text-[22px] text-[#4e525a] dark:text-gray-300">
             Product listing with API handling and modern layout.
           </p>
-          <p className="md:text-[22px] text-[#4e525a]">
+          <p className="md:text-[22px] text-[#4e525a] dark:text-gray-300">
             Build with ReactJS and TailwindCSS.
           </p>
         </div>
 
-        <div data-aos='fade-up' className="project4 mt-20 bg-white rounded-2xl">
+        <div data-aos="fade-up" className="project4 mt-20 dark:bg-gray-800 p-3 bg-white rounded-2xl">
           <div className="w-full shadow shadow-gray-400 p-5 flex justify-center items-center rounded-2xl">
             <a href="https://github.com/ataullaharabzai" className="">
               <div>
@@ -121,11 +128,11 @@ function Projects() {
           <p className="text-2xl md:text-5xl mt-3 font-semibold">
             YouTube Clone
           </p>
-          <p className="md:text-[22px] text-[#4e525a]">
+          <p className="md:text-[22px] text-[#4e525a] dark:text-gray-300">
             A YouTube clone with three pages and basic DOM manipulation and
             clean layout.
           </p>
-          <p className="md:text-[22px] text-[#4e525a]">
+          <p className="md:text-[22px] text-[#4e525a] dark:text-gray-300">
             Build with HTML5, CSS3 and JavaScript.
           </p>
         </div>
